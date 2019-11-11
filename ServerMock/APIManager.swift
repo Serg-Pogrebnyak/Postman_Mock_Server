ba79b67f9f5c57cf390c7ca06aca36b28da16c70
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2019-11-11 11:04:00 +0200

    added api manager

---
+++ ServerMock/APIManager.swift
@@ -0,0 +1,18 @@
+//
+//  APIManager.swift
+//  ServerMock
+//
+//  Created by Sergey Pogrebnyak on 11.11.2019.
+//  Copyright © 2019 Sergey Pogrebnyak. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct APIManager {
+    static var shared = APIManager()
+
+    func login() {
+        print("login")
+    }
+}
--- ServerMock/ViewController.swift
+++ ServerMock/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet fileprivate weak var loginTextField: UITextField!
+    @IBOutlet fileprivate weak var passTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    //MARK: Actions
+    @IBAction func didTapSignInButton(_ sender: Any) {
+        APIManager.shared.login()
+    }
 }
 
