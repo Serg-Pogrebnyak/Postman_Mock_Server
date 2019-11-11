4301747f2c70cc59b69cf7ccd6b33cb220bdce91
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2019-11-11 10:16:37 +0200

    empty project

---
+++ ServerMock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServerMock
+//
+//  Created by Sergey Pogrebnyak on 28.10.2019.
+//  Copyright © 2019 Sergey Pogrebnyak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
